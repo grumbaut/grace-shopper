9e803634d2197e96a6f8d82576c0d71396a217ba
Gabriel Rumbaut <grumbaut@gmail.com>
2018-04-18 13:56:49 -0400

    added react components

--- client/components/Main.js
+++ client/components/Main.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { Switch, Route, HashRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 
+import Nav from './Nav';
+import Home from './Home';
+
 class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [],
+      products: []
+    };
+  }
+
   render() {
     return (
-      <hr />
+      <HashRouter>
+        <Nav />
+        <div className='container-fluid'>
+          { /* Container-fluid is from bootstrap and imposes margins. Placing it here allows the navbar
+          to stretch across the screen. */ }
+          <Switch>
+            <Route path='/' component={ Home } />
+          </Switch>
+        </div>
+      </HashRouter>
     );
   }
 }
--- client/index.js
+++ client/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import Main from '../components/Main';
 
 ReactDOM.render(
-  <Main />,
+  <Provider>
+    <Main />
+  </Provider>,
   document.getElementById('root')
 );
