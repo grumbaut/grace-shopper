e7b5fc8fbeb7bb74f89212f8684165c0f25c9d9e
Alan Fleming <alalfleming@gmail.com>
2018-04-18 21:55:02 -0400

    faker dependency added for seeding

--- server/db/Order.js
+++ server/db/Order.js
@@ -2,9 +2,12 @@ const conn = require('./conn');
 const { Sequelize } = conn;
 
 const Order = conn.define('order', {
+  cart: Sequelize.BOOL,
+  total: Sequelize.INTEGER
   //isCart : BOOL
   // Checkout, review, payment, checkout complete
+
 });
 
-
+//will have a UserId
 module.exports = Order;
