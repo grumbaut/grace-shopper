0bdc09991a8ae49fdebfc1008a01e4471d9f6225
Gabriel Rumbaut <grumbaut@gmail.com>
2018-04-18 21:12:01 -0400

    added products component

---
+++ client/components/Products.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Products extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filter: null
+    };
+  }
+
+  render() {
+    return (
+      <div>
+          <hr />
+      </div>
+    );
+  }
+}
+
+export default Products;
