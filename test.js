341251038ddfb90c894e82deab99e1146ad59e10
Alan Fleming <alalfleming@gmail.com>
2018-04-19 14:58:15 -0400

    testing page runs into sequelize errors

---
+++ test.js
@@ -0,0 +1,55 @@
+const expect = require('chai').expect;
+const db = require('./server/db');
+const { Product, Category, User } = db.models;
+
+
+describe('models', ()=> {
+  beforeEach(()=> {
+    return db.syncAndSeed();
+  });
+  describe('seeded data', ()=> {
+    let products;
+    beforeEach(()=> {
+      return Product.findAll({})
+      .then( _products => products = _products);
+    });
+    it('there is 1 product in the database', ()=> {
+      expect(products.length).to.equal(1);
+    });
+    // describe('User data', ()=> {
+    //   let users;
+    //   beforeEach(()=> {
+    //     return User.findAll({})
+    //     .then( _users => users = _users);
+    //   });
+    //   it('we have users.', ()=> {
+    //     expect(users).to.be.ok;
+    //   });
+    // });
+  });
+});
+
+// describe('Products', function() {
+//   describe('Number of products', function() {
+//     it('should return 3 when searching database for products', function(){
+//       assert.equal(-1, Product.findAll().length);
+//     });
+//   });
+// });
+
+// describe('Categories', function() {
+//   describe('Number of categories', function() {
+//     it('should return 3 when searching database for categories', function(){
+//       assert.equal(-1, Category.findAll().length);
+//     });
+//   });
+// });
+
+// describe('Users', function() {
+//   describe('Number of users', function() {
+//     it('should return 3 when searching database for users', function(){
+//       assert.equal(-1, User.findAll().length);
+//     });
+//   });
+// });
+
