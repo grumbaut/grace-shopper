8346fdec870b3deee977e4c4ceb2211c478e7cf8
Gabriel Rumbaut <grumbaut@gmail.com>
2018-04-19 19:37:21 -0400

    fixed bug in server

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 
 router.use('/users', require('./users'));
 router.use('/categories', require('./categories'));
-router.use('/lineitems', require('./lineitems'));
+router.use('/lineitems', require('./lineItems'));
 router.use('/orders', require('./orders'));
 router.use('/products', require('./products'));
 
