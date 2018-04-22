c69710d1e43d59fdf9fad16631c9a0b4f48245db
Alan Fleming <alalfleming@gmail.com>
2018-04-22 19:45:56 -0400

    refactored with the userMap

--- test/server/models.spec.js
+++ test/server/models.spec.js
@@ -86,84 +86,85 @@ describe('User model', () => {
 })
 
 //AUTHENTICATION TESTS
+let userMap;
+const jwt = require('jwt-simple');
+const KEY = process.env.JWT_KEY;
 
-//This is how Prof did it, but if you console users, you only get a list of products (no categories even)
-    //   .then( users => {
-    //     userMap = users.reduce((memo, user)=> {
-    //       memo[user.firstName] = user
-    //       return memo;
-    //     }, {});
-    //     });
-
-let users;
-const jwt = require('jwt-simple')
-const KEY = process.env.JWT_KEY
-
-describe ('authentication', () => {
-  
-  beforeEach (()=> {
+describe('authentication', () => {
+  beforeEach(() => {
     return User.findAll({})
-      .then(_users => users = _users)
+      .then(users => {
+        userMap = users.reduce((memo, user) => {
+          memo[user.firstName] = user;
+          return memo;
+        }, {});
+      });
   });
-  describe ('seeded data', ()=> {
-    it('Alice, Bob, Cat exist', ()=> {
-      expect (users[0].firstName).to.be.equal('Alice')
-    })
-  });
-  describe('authenticate', ()=> {
-    it('authenticating with correct credentials returns a token', ()=> {
-        const Alice = users[0]
-        const expectedToken = jwt.encode({
-            id: Alice.id
-        }, KEY);
-        return User.authenticate({
-          firstName: Alice.firstName,
-          password: Alice.password
-        })
-        .then( token => expect(token).to.equal(expectedToken));
+  describe('seeded data', () => {
+    it('Alice, Bob, Cat exist', () => {
+      expect(userMap.Alice.firstName).to.be.equal('Alice');
     });
-    it('authenticating with incorrect credentials will throw an error with a 401 status', ()=> {
-      const Alice = users[0]
+  });
+
+  describe('authenticate', () => {
+    it('authenticating with correct credentials returns a token', () => {
+      const Alice = userMap.Alice;
+      const expectedToken = jwt.encode({
+        id: Alice.id
+      }, KEY);
+      return User.authenticate({
+        firstName: Alice.firstName,
+        password: Alice.password
+      })
+        .then(token => expect(token).to.equal(expectedToken));
+    });
+
+    it('authenticating with incorrect credentials will throw an error with a 401 status', () => {
+      const Alice = userMap.Alice;
       return User.authenticate({
         firstName: Alice.firstName,
         password: 'bad password'
       })
-      .then(()=> { throw 'no!!' })
-      .catch( ex => expect(ex.status).to.equal(401))
-    })
+        .then(() => { throw 'no!!' })
+        .catch(ex => expect(ex.status).to.equal(401));
+    });
   });
-  describe('exchanging a token', ()=> {
-    it('a valid token which matches a user returns the user', ()=> {
-      const Alice = users[0]
+
+  describe('exchanging a token', () => {
+    it('a valid token which matches a user returns the user', () => {
+      const Alice = userMap.Alice;
       const token = jwt.encode({
         id: Alice.id
-      }, KEY)
+      }, KEY);
       return User.exchangeTokenForUser(token)
-        .then (user => expect(user.firstName).to.equal(Alice.firstName))
+        .then(user => expect(user.firstName).to.equal(Alice.firstName));
     });
-    it('a valid token which does not match a user will return an error with a 401 status', ()=> {
+
+    it('a valid token which does not match a user will return an error with a 401 status', () => {
       const token = jwt.encode({
         id: User.build().id
-      }, KEY)
+      }, KEY);
       return User.exchangeTokenForUser(token)
-        .then( user => { throw 'no!' })
-        .catch( ex => expect(ex.status).to.equal(401))
-    })
-    it('a invalid token will return an error with a 401 status', ()=> {
-      const token = jwt.encode({
-        id: User.build().id
-      }, 'some silly key')
-      return User.exchangeTokenForUser(token)
-        .then( user => { throw 'no!!' })
-        .catch( ex => expect(ex.status).to.equal(401))
-    })
-    it('a valid token with the wrong data type for a userId with will return a 401 status', ()=> {
+        .then(()=> { throw 'no!';})
+        .catch(ex => expect(ex.status).to.equal(401));
+    });
+
+    it('a invalid token will return an error with a 401 status', () => {
       const token = jwt.encode({
         id: User.build().id
       }, 'some silly key');
       return User.exchangeTokenForUser(token)
-        .then (user => { throw 'no!!!' })
-        .catch( ex => expect(ex.status).to.equal(401))
-    })
+      .then(()=> {throw 'no!';})
+      .catch(ex => expect(ex.status).to.equal(401));
+    });
+
+    it('a valid token with the wrong data type for a userId with will return a 401 status', () => {
+      const token = jwt.encode({
+        id: User.build().id
+      }, 'some silly key');
+      return User.exchangeTokenForUser(token)
+      .then(()=> {throw 'no!';})
+      .catch(ex => expect(ex.status).to.equal(401));
+    });
   });
-})
+});
