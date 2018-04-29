94c99933c0232d16895b8653fe44a80472ddc7b3
AnnaMcGrane <annamaymcgrane@gmail.com>
2018-04-29 10:21:32 -0400

    admin button appears

--- client/components/Product.js
+++ client/components/Product.js
@@ -7,22 +7,19 @@ const Product = ({ user, product, categories, id }) => {
   if (!product) {
     return null;
   }
-  // componentWillReceiveProps(){
-
-  // }
   const categoryOfThisProduct = categories.find(category=> category.id === product.categoryId);
   return (
     <div>
       {/*<ProductCard product={product} />*/}
-      <div>
         <h1>{ product.name }</h1>
         <img src = { product.imageUrl } width={400} />
         <h2>{`$${product.price}`}</h2>
         <p>{ product.description }</p>          
-      </div>
       <p>{ product.name } is in our <Link to={`/categories/${categoryOfThisProduct.id}`}>{categoryOfThisProduct.name}</Link> category</p>
       <ul>
-
+          {
+            user.isAdmin === true ?  <button>Edit</button> : <button>Add to cart</button>
+          }
       </ul>
     </div>
   )
@@ -30,11 +27,9 @@ const Product = ({ user, product, categories, id }) => {
 
 const mapState = ({ products, categories, user }, { id })=> {
   const product = products.find( product => product.id === id );
-  // const categoryOfThisProduct = categories.find(category=> category.id === product.categoryId);
   return {
     product,
     categories,
-    // categoryOfThisProduct,
     user
   };
 };
