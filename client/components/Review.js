41eb80bd134a917002b764e06f4352443353bac6
Alan Fleming <alalfleming@gmail.com>
2018-04-30 20:16:38 -0400

    star image added

--- client/components/Review.js
+++ client/components/Review.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 const Review = (props) => {
+  const star = '/public/images/star.png';
   const review = props.review;
   return (
     <div>
+      <div>Rating: <img src={star} /></div>
       {review.content}
       <p>Reviewer: {review.user.name}</p>
     </div>
