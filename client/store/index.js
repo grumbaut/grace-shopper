e8206e661c8aaaeacc27b091b460a14f18f82813
Gabriel Rumbaut <grumbaut@gmail.com>
2018-05-02 09:59:45 -0400

    fixed breaking bug

--- client/store/index.js
+++ client/store/index.js
@@ -4,8 +4,10 @@ import thunk from 'redux-thunk';
 import categories from './categories';
 import products from './products';
 import user from './sessions';
+import cart from './cart';
+import reviews from './reviews';
 
-const reducers = combineReducers({ categories, products, user });
+const reducers = combineReducers({ categories, products, user, cart, reviews });
 const middleware = applyMiddleware(thunk, logger);
 
 const store = createStore(reducers, middleware);
@@ -14,5 +16,5 @@ export default store;
 export * from './categories';
 export * from './products';
 export * from './sessions';
+export * from './reviews';
 export * from './cart';
-
--- server/db/index.js
+++ server/db/index.js
@@ -4,6 +4,8 @@ const Product = require('./Product');
 const User = require('./User');
 const Order = require('./Order');
 const LineItem = require('./LineItem');
+const Review = require('./Review');
+const fake = require('faker');
 
 Product.belongsTo(Category);
 Product.hasMany(LineItem);
@@ -13,7 +15,10 @@ LineItem.belongsTo(Product);
 Order.hasMany(LineItem);
 Order.belongsTo(User);
 User.hasMany(Order);
-
+Review.belongsTo(Product);
+Product.hasMany(Review);
+Review.belongsTo(User);
+User.hasMany(Review);
 
 const syncAndSeed = ()=>{
   return conn.sync({ force: true })
@@ -25,16 +30,28 @@ const syncAndSeed = ()=>{
         Product.create({ name: 'Vase', description: 'Porcelain longnecked vase, ideal for roses.', imageUrl: '/images/vase.jpg', price: 31.95 }),
         Product.create({ name: 'Vanilla Diffuser', description: 'A room diffuser with reeds and vanilla oil', imageUrl: '/images/vanilladiffuser.jpg', price: 6.85 }),
         User.create({firstName: 'Alice', lastName: 'Buyer', email: 'alice@wonderland.com', isAdmin: 'false', password: 'ALICE'}),
+        Review.create({ content: fake.lorem.paragraph(), rating: 5 }),
+        Review.create({ content: fake.lorem.paragraph(), rating: 4 }),
+        Review.create({ content: fake.lorem.paragraph(), rating: 3 }),
+        Review.create({ content: fake.lorem.paragraph(), rating: 1 }),
         User.create({firstName: 'Bob', lastName: 'Bill', email: 'bob@wonderland.com', isAdmin: 'false', password: 'BOB'}),
         User.create({firstName: 'Cat', lastName: 'Purchase', email: 'cat@wonderland.com', isAdmin: 'false', password: 'CAT'}),
       ]);
     })
-    .then(([ category1, category2, product1, product2, product3, user1])=>{
+    .then(([ category1, category2, product1, product2, product3, user1, rev1, rev2, rev3, rev4, user2, user3])=>{
       return Promise.all([
         product1.setCategory(category1),
         product2.setCategory(category2),
         product3.setCategory(category2),
-        Order.findOrCreateCart(user1.id)
+        Order.findOrCreateCart(user1.id),
+        rev1.setUser(user1),
+        rev1.setProduct(product1),
+        rev2.setUser(user2),
+        rev2.setProduct(product1),
+        rev3.setUser(user3),
+        rev3.setProduct(product1),
+        rev4.setUser(user3),
+        rev4.setProduct(product2)
       ]);
     })
     .then(([product1, product2, product3, order]) => {
@@ -53,6 +70,7 @@ module.exports = {
     Product,
     User,
     LineItem,
-    Order
+    Order,
+    Review
   }
 };
