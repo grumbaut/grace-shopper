2b5829ef5c55dcc696e18c0e042ecee117ecd018
Alan Fleming <alalfleming@gmail.com>
2018-05-03 08:36:44 -0400

    commit

--- server/db/seedProducts.js
+++ server/db/seedProducts.js
@@ -89,12 +89,144 @@ const productArray = [
     imageUrl: '/images/dragonwineglass.jpg',
     price: 26.25
   },
-  // {
-  //   name: 'Vanilla Diffuser',
-  //   description: 'A room diffuser with reeds and vanilla oil',
-  //   imageUrl: '/images/vanilladiffuser.jpg',
-  //   price: 6.85
-  // },
+  {
+    name: 'Floral Place Mat',
+    description: 'Fancy floral place mat goes great with any meal.',
+    imageUrl: '/images/floralplacemat.jpg',
+    price: 12.25
+  },
+  {
+    name: 'Floral Serving Platter',
+    description: 'Impress your guests with your presentation by using this floral serving platter.',
+    imageUrl: '/images/floralservingplatter.jpg',
+    price: 22.50
+  },
+  {
+    name: 'Frying Pan Set',
+    description: 'This frying pan set will complete and kitchen.',
+    imageUrl: '/images/fryingpanset.jpg',
+    price: 58.65
+  },
+  {
+    name: 'Pheasant Platter',
+    description: 'Gold pheasant platter will impress your guests.',
+    imageUrl: '/images/goldpheasantplatter.jpg',
+    price: 33.65
+  },
+  {
+    name: 'Lemon Platter',
+    description: 'Serving platter with an artful twist.',
+    imageUrl: '/images/lemonplatter.jpg',
+    price: 28.65
+  },
+  {
+    name: 'Measuring Cups',
+    description: 'Measure and mix up something special with this measuring cup set.',
+    imageUrl: '/images/measuringcups.jpg',
+    price: 20.65
+  },
+  {
+    name: 'Mini Bowl Set',
+    description: 'The mini bowl set is great for hosting parties.',
+    imageUrl: '/images/minibowlset.jpg',
+    price: 35.65
+  },
+  {
+    name: 'Mugs',
+    description: 'Enjoy coffee or tea in these sturdy mugs.',
+    imageUrl: '/images/mugset.jpg',
+    price: 22.65
+  },
+  {
+    name: 'Petal Place Mat',
+    description: 'Keep your table safe with this petal place mat.',
+    imageUrl: '/images/petalplacemat.jpg',
+    price: 10.65
+  },
+  {
+    name: 'Pizza Cutter',
+    description: 'Cut up your homemade pizza in style with this pizza cutter.',
+    imageUrl: '/images/pizzacutter.jpg',
+    price: 16.65
+  },
+  {
+    name: 'Pizza Maker',
+    description: 'This pizza maker takes the headache out of making pizzas.',
+    imageUrl: '/images/pizzamaker.jpg',
+    price: 42.65
+  },
+  {
+    name: 'Pizza Pan',
+    description: 'Pizza baking is clean and easy with this pizza pan.',
+    imageUrl: '/images/pizzapan.jpg',
+    price: 20.65
+  },
+  {
+    name: 'Pizza Slicer',
+    description: 'Slice your pizza with ease with this pizza slicer.',
+    imageUrl: '/images/pizzaslicer.jpg',
+    price: 20.65
+  },
+  {
+    name: 'Pleated Platter Blue',
+    description: 'Platter your meals and keep them fresh.',
+    imageUrl: '/images/pleatedplatterblue.jpg',
+    price: 18.65
+  },
+  {
+    name: 'Pleated Platter',
+    description: 'Platter your meals and keep them fresh.',
+    imageUrl: '/images/pleatedplatterwhite.jpg',
+    price: 18.65
+  },
+  {
+    name: 'Sauce Pot',
+    description: 'Simmer you favorite sauce on the stove with this sauce pot.',
+    imageUrl: '/images/saucepot.jpg',
+    price: 24.65
+  },
+  {
+    name: 'Serving Bowls',
+    description: 'Serve up your favorite soup in these lovely bowls.',
+    imageUrl: '/images/servingbowlset.jpg',
+    price: 25.65
+  },
+  {
+    name: 'Blue Serving Bowls',
+    description: 'Serve up your favorite soup in these lovely bowls.',
+    imageUrl: '/images/servingbowlsetblue.jpg',
+    price: 25.65
+  },
+  {
+    name: 'Soup Pot',
+    description: 'Stir up your best soup in this lovely soup pot.',
+    imageUrl: '/images/souppot.jpg',
+    price: 20.25
+  },
+  {
+    name: 'Square Dipping Bowls',
+    description: 'Dipping bowls for all your dipping needs.',
+    imageUrl: '/images/squaredipbowls.jpg',
+    price: 20.65
+  },
+  {
+    name: 'Square Plate Set',
+    description: 'Square plates are great for small appetizers.',
+    imageUrl: '/images/squareplateset.jpg',
+    price: 20.65
+  },
+  {
+    name: 'Steak Knife Set',
+    description: 'Steak dinners require a good set of steak knifes.',
+    imageUrl: '/images/steakknifeset.jpg',
+    price: 60.65
+  },
+  {
+    name: 'Sugar and Creamer',
+    description: 'Sugar and creamer container for your breakfast coffee.',
+    imageUrl: '/images/sugarcreamer.jpg',
+    price: 18.65
+  }
 ];
 
 module.exports = productArray;
