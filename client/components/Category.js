92fa99e8fbe86c07421b4348a6e1d377a5e8822a
Gabriel Rumbaut <grumbaut@gmail.com>
2018-05-04 23:27:04 -0400

    fixed styling on category display page

--- client/components/Category.js
+++ client/components/Category.js
@@ -14,35 +14,31 @@ const Category = ({ category, categories, products, id, productsOfThisCategory }
   const priorCategoryId = priorCategoryIndex !== -1 ? categories[priorCategoryIndex].id : categories[lastCategoryIndex].id;
 
   return (
-    <div className='container'>
-      <div className='row'>
-        <div className='col'>
-          <h1>{ category.name }</h1>
-          <p>Number of products in {category.name}: {productsOfThisCategory.length}</p>
-          <p>Products:</p>
-          {productsOfThisCategory.length === 0 ? (<p>There are no products in this category yet</p>):(        
-            <ul>          
-           {
-             productsOfThisCategory.map(product => {
-               return (
-                 <div key={product.id}>                   
-                   <ProductCard product={product} />                   
-                 </div>
-               )
-             })
-           }         
-           </ul>)}
+    <div>
+      <h1>{ category.name }</h1>
+      <p>Number of products in {category.name}: {productsOfThisCategory.length}</p>
+      <p>Products:</p>
+      {productsOfThisCategory.length === 0 ?
+        <p>There are no products in this category yet</p>
+        :
+        <div className='row'>
+          {
+            productsOfThisCategory.map(product => {
+              return (
+                <ProductCard product={product} key={ product.id } />
+              );
+            })
+          }
         </div>
-        <div className='col'>
-            <Link to={`/categories/${priorCategoryId}`}><button>Prior</button></Link>
-            <Link to={`/categories/${nextCategoryId}`}><button>Next</button></Link>
-        </div>
-      </div>      
+      }
+      <Link to={`/categories/${priorCategoryId}`}><button>Prior</button></Link>
+      <Link to={`/categories/${nextCategoryId}`}><button>Next</button></Link>
     </div>
   )
 }
 
-const mapStateToProps = ({ categories, products }, { id })=> {
+const mapStateToProps = ({ categories, products }, { match })=> {
+  const id = match.params.id * 1;
   const category = categories.find( category => category.id === id );
   const productsOfThisCategory = products.filter( product => product.categoryId === id);
   return {
