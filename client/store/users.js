06399ce2580366328ff814ce18d95a113118fe4c
Edward Timmer <edtimmer@gmail.com>
2018-05-06 16:18:53 -0400

    some edits to store users js

--- client/store/users.js
+++ client/store/users.js
@@ -14,47 +14,46 @@ const deleteUserInStore = user => {
   return action;
 };
 
-
 const updateUserInStore = user => {
     const action = { type: UPDATE_USER, user };
     return action;
 };
 
+const reducer = (state = [], action) => {
+  switch (action.type) {
+  case GOT_USERS:
+    return action.users;
+  case DELETE_USER:
+    return state.filter(user => user.id !== action.user.id);
+  case UPDATE_USER:
+    return state.map(user => user.id === action.user.id ? action.user : user);
+  default:
+    return state;
+  }
+};
+
 export const getUsers = () => (
-    dispatch => (
-      axios.get('/api/users')
-        .then(res => res.data)
-        .then(users => dispatch(addUsersToStore(users)))
-    )
+  dispatch => (
+    axios.get('/api/users')
+      .then(res => res.data)
+      .then(users => dispatch(addUsersToStore(users)))
+  )
 );
 
 export const saveUser = (user) => (
-    console.log(user, 'saveUser'),
-        dispatch => (
-            axios.put(`api/users/${user.id}`, user)
-              .then(result => result.data)
-              .then(user => dispatch(updateUserInStore(user))))
+    // console.log(user, 'saveUser'),
+  dispatch => (
+    axios.put(`api/users/${user.id}`, user)
+      .then(result => result.data)
+      .then(user => dispatch(updateUserInStore(user))))
 );
 
 export const deleteUser = (user) => (
-    console.log('delete', user),
+    // console.log('delete', user),
     dispatch => (
       axios.delete(`api/users/${user.id}`)
-        .then( () => dispatch(deleteProductInStore(user)))
+        .then( () => dispatch(deleteUserInStore(user)))
     )
   );
 
-const reducer = (state = [], action) => {
-    switch (action.type) {
-    case GOT_USERS:
-      return action.users;
-    case DELETE_USER:
-      return state.filter(user => user.id !== action.user.id);
-    case UPDATE_USER:
-      return state.map(user => user.id === action.user.id ? action.user : user);
-    default:
-      return state;
-    }
-  };
-
-export default reducer
+export default reducer;
