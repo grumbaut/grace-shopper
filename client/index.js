8aa0e6e904142f9f5668ba0d312c8e5c4868bc76
Gabriel Rumbaut <grumbaut@gmail.com>
2018-05-12 11:36:49 -0400

    added stripe card form

--- client/index.js
+++ client/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import Main from './components/Main';
+import { StripeProvider } from 'react-stripe-elements';
 
 ReactDOM.render(
   <Provider store={ store }>
-    <Main />
+    <StripeProvider apiKey='pk_test_AhLiCUGnPSZ5IySTH3Givqd0'>
+      <Main />
+    </StripeProvider>
   </Provider>,
   document.getElementById('root')
 );
