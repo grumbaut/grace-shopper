120771927c8c2a4537e1419a446fff6780cb194e
Alan Fleming <alalfleming@gmail.com>
2018-05-14 23:52:21 -0400

    loading fake user data to test map

---
+++ client/components/mapData.js
@@ -0,0 +1,205 @@
+//Hard coded user data to test D3 map
+const data = {
+  "AZ": {
+      "fillKey": "noMembers",
+      "members": 0
+  },
+  "CO": {
+    "fillKey": "noMembers",
+    "members": 0
+  },
+  "DE": {
+    "fillKey": "noMembers",
+    "members": 0
+  },
+  "FL": {
+      "fillKey": "overFive",
+      "members": 6
+  },
+  "GA": {
+    "fillKey": "overFive",
+    "members": 6
+  },
+  "HI": {
+    "fillKey": "noMembers",
+    "members": 0
+  },
+  "ID": {
+    "fillKey": "overTen",
+    "members": 15
+  },
+  "IL": {
+    "fillKey": "overTwenty",
+    "members": 25
+  },
+  "IN": {
+      "fillKey": "oneMember",
+      "members": 1
+  },
+  "IA": {
+    "fillKey": "oneMember",
+    "members": 1
+  },
+  "KS": {
+    "fillKey": "oneMember",
+    "members": 1
+  },
+  "KY": {
+    "fillKey": "noMembers",
+    "members": 0
+  },
+  "LA": {
+    "fillKey": "noMembers",
+    "members": 0
+  },
+  "MD": {
+    "fillKey": "oneMember",
+    "members": 1
+  },
+  "ME": {
+    "fillKey": "noMembers",
+    "members": 0
+  },
+  "MA": {
+    "fillKey": "noMembers",
+    "members": 0
+  },
+  "MN": {
+    "fillKey": "noMembers",
+    "members": 0
+  },
+  "MI": {
+    "fillKey": "overTen",
+    "members": 15
+  },
+  "MS": {
+    "fillKey": "overTen",
+    "members": 15
+  },
+  "MO": {
+    "fillKey": "overTen",
+    "members": 15
+  },
+  "MT": {
+    "fillKey": "noMembers",
+    "members": 0
+  },
+  "NC": {
+    "fillKey": "noMembers",
+    "members": 0
+  },
+  "NE": {
+    "fillKey": "noMembers",
+    "members": 0
+  },
+  "NV": {
+    "fillKey": "noMembers",
+    "members": 0
+  },
+  "NH": {
+    "fillKey": "noMembers",
+    "members": 0
+  },
+  "NJ": {
+    "fillKey": "overTen",
+    "members": 15
+  },
+  "NY": {
+    "fillKey": "overTwenty",
+    "members": 25
+  },
+  "ND": {
+    "fillKey": "overFive",
+    "members": 6
+  },
+  "NM": {
+    "fillKey": "overFive",
+    "members": 6
+  },
+  "OH": {
+    "fillKey": "overFive",
+    "members": 6
+  },
+  "OK": {
+    "fillKey": "oneMember",
+    "members": 1
+  },
+  "OR": {
+    "fillKey": "oneMember",
+    "members": 1
+  },
+  "PA": {
+    "fillKey": "oneMember",
+    "members": 1
+  },
+  "RI": {
+    "fillKey": "oneMember",
+    "members": 1
+  },
+  "SC": {
+    "fillKey": "oneMember",
+    "members": 1
+  },
+  "SD": {
+    "fillKey": "noMembers",
+    "members": 0
+  },
+  "TN": {
+    "fillKey": "noMembers",
+    "members": 0
+  },
+  "TX": {
+    "fillKey": "overTwenty",
+    "members": 25
+  },
+  "UT": {
+    "fillKey": "overFive",
+    "members": 6
+  },
+  "WI": {
+    "fillKey": "overFive",
+    "members": 6
+  },
+  "VA": {
+    "fillKey": "oneMember",
+    "members": 1
+  },
+  "VT": {
+    "fillKey": "noMembers",
+    "members": 0
+  },
+  "WA": {
+    "fillKey": "noMembers",
+    "members": 0
+  },
+  "WV": {
+    "fillKey": "noMembers",
+    "members": 0
+  },
+  "WY": {
+    "fillKey": "noMembers",
+    "members": 0
+  },
+  "CA": {
+    "fillKey": "overTwenty",
+    "members": 25
+  },
+  "CT": {
+      "fillKey": "overTen",
+      "members": 14
+  },
+  "AK": {
+    "fillKey": "noMembers",
+    "members": 0
+  },
+  "AR": {
+    "fillKey": "noMembers",
+    "members": 0
+  },
+  "AL": {
+    "fillKey": "noMembers",
+    "members": 0
+  }
+};
+
+module.exports = data;
